9e51373b6e91ca49fbfc259d3eef27afe92b38bd
Dustin Lyons <dustin@zeroedlabs.com>
2021-10-12 07:22:56 -0400

    Revert comments in home-bash-service-type

--- build/felix-home.scm
+++ build/felix-home.scm
@@ -37,33 +37,33 @@
 		("COMPLETION_WAITING_DOTS" . #t)
 		("OSH" . "$HOME/Resources/code/oh-my-bash")
 		("GUIX_PROFILE" . "$HOME/.guix-profile")
-		("OSH_THEME" . "agnoster"))))
+		("OSH_THEME" . "agnoster")))
 
-;;(service home-bash-service-type
-;;        (home-bash-configuration
-;;          (guix-defaults? #f)
-;;          (bashrc '("# Export 'SHELL' to child processes.
-;;# Programs such as 'screen' honor it and otherwise use /bin/sh.
-;;export SHELL
-;;alias ls='ls --color'
+(service home-bash-service-type
+        (home-bash-configuration
+          (guix-defaults? #f)
+          (bashrc '("# Export 'SHELL' to child processes.  
+# Programs such as 'screen' honor it and otherwise use /bin/sh.
+export SHELL
+alias ls='ls --color'
     
-;;# We are being invoked from a non-interactive shell.  If this
-;;# is an SSH session (as in \"ssh host command\"), source
-;;# /etc/profile so we get PATH and other essential variables.
-;;if [[ $- != *i* ]]
-;;then
-;;[[ -n \"$SSH_CLIENT\" ]] && source \"$GUIX_PROFILE/etc/profile\"
+# We are being invoked from a non-interactive shell.  If this
+# is an SSH session (as in \"ssh host command\"), source
+# /etc/profile so we get PATH and other essential variables.
+if [[ $- != *i* ]]
+then
+[[ -n \"$SSH_CLIENT\" ]] && source \"$GUIX_PROFILE/etc/profile\"
     
-;;# Don't do anything else.
-;;return
-;;fi
+# Don't do anything else.
+return
+fi
 
-;;# System wide configuration
-;;source /etc/bashrc
+# System wide configuration
+source /etc/bashrc
 
-;;# oh-my-BASH!
-;;export OSH=$HOME/Resources/code/oh-my-bash
-;;source $OSH/oh-my-bash.sh")))))
+# oh-my-BASH!
+export OSH=$HOME/Resources/code/oh-my-bash
+source $OSH/oh-my-bash.sh")))))
 
 ;; Generates home-files-services for each dotfile defined
 ;; throughout configuration inheritance
