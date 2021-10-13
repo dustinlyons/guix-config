51868b67792271f8252c126741b753854249cd04
Dustin Lyons <dustin@zeroedlabs.com>
2021-10-12 22:11:35 -0400

    Remove build from repo

--- build/felix-os.scm
+++
@@ -1,9 +0,0 @@
-(define-module (felix-os)
-  #:use-module (desktop)
-  #:use-module (gnu services)
-  #:use-module (gnu system)
-  #:use-module (gnu packages))
-
-(operating-system
- (inherit desktop-operating-system)
- (host-name "felix"))
